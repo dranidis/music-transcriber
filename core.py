1429a71a44580f5766585820cb093834eba18ec9
D Dranidis <dranidis@gmail.com>
2024-06-15 11:52:17 +0300

    Fix some linting errors

--- core.py
+++ core.py
@@ -1,14 +1,29 @@
+"""
+This module contains functions and a class for handling audio data. 
+
+It includes functions to read an audio file, get a specific segment of an audio array, 
+and stretch the audio array. It also contains the Core class (definition not fully shown here).
+"""
+
 import pygame
 from pydub import AudioSegment
 import numpy as np
 import librosa
 
-# a function that reads an audio file and returns the audio array, sample rate, and number of channels
+
 def read_audio_file(file_path):
+    """
+    Reads an audio file and returns the audio array, sample rate, and number of channels.
+
+    Args:
+        file_path (str): The path to the audio file.
+
+    Returns:
+        tuple: A tuple containing the audio array, sample rate, and number of channels.
+    """
     audio_segment = AudioSegment.from_file(file_path)
     sample_rate = audio_segment.frame_rate
     num_channels = audio_segment.channels
-    sample_width = audio_segment.sample_width
     raw_data = audio_segment.raw_data
     audio_array = np.frombuffer(raw_data, dtype=np.int16)
     if num_channels == 2:
@@ -16,12 +31,34 @@ def read_audio_file(file_path):
 
     return audio_array, sample_rate, num_channels
 
-# a function that returns the audio array starting from start_time and lasting duration seconds
+
 def get_audio_array(audio_array, start_time, duration, sample_rate):
+    """
+    Returns the audio array starting from start_time and lasting duration seconds.
+
+    Args:
+        audio_array (np.array): The audio array.
+        start_time (float): The start time in seconds.
+        duration (float): The duration in seconds.
+        sample_rate (int): The sample rate.
+
+    Returns:
+        np.array: The segment of the audio array.
+    """
     return audio_array[int(start_time * sample_rate):int((start_time + duration) * sample_rate)]
 
-# a function that takes an audio array and a slow_down_rate and returns a stretched audio array
+
 def stretch_audio(audio_array, slow_down_rate):
+    """
+    Stretches the audio array by a given rate.
+
+    Args:
+        audio_array (np.array): The audio array.
+        slow_down_rate (float): The rate to slow down the audio.
+
+    Returns:
+        np.array: The stretched audio array.
+    """
     # prepare the audio by converting the audio array to float32
     audio = audio_array.astype(np.float32) / 32767.0
     # stretch the audio
@@ -33,14 +70,36 @@ def stretch_audio(audio_array, slow_down_rate):
     #     stretched_audio_int16 = np.stack((stretched_audio_int16, stretched_audio_int16), axis=-1)
     return stretched_audio_int16
 
+
 class Core:
+    """
+    The Core class handles the audio data and the interaction with the Pygame mixer.
+
+    Attributes:
+        root: The root widget of the application.
+        plot: The plot widget of the application.
+        beats (list): A list to store the beats.
+        measures (list): A list to store the measures.
+        original_data (np.array): The original audio data.
+        playing_data (np.array): The audio data currently being played.
+        sample_rate (int): The sample rate of the audio data.
+        num_channels (int): The number of channels in the audio data.
+    """
+
     def __init__(self, root, plot):
+        """
+        Initializes the Core class with the root and plot widgets, initializes the Pygame mixer,
+        and initializes the beats, measures, and audio data attributes.
+
+        Args:
+            root: The root widget of the application.
+            plot: The plot widget of the application.
+        """
         self.root = root
         self.plot = plot
 
         # Initialize Pygame mixer
         pygame.mixer.init()
-        pygame.init()
 
         self.beats = []
         self.measures = []
@@ -58,25 +117,32 @@ class Core:
         self.loop_end = None
         self.start_play_time = None
 
-
         self.paused_at = 0
 
     def on_closing(self):
+        """
+        Stops the music playback and quits Pygame when the application is closed.
+        """
         pygame.mixer.stop()  # Stop music playback
-        pygame.quit()
+        pygame.quit()  # pyLint: disable=no-member
 
     def load_mp3_from_file_path(self, file_path):
-        self.original_data, self.sample_rate, self.num_channels = read_audio_file(file_path)
+        """
+        Loads an MP3 file from the given file path and displays the waveform.
+        """
+
+        self.original_data, self.sample_rate, self.num_channels = read_audio_file(
+            file_path)
         self.playing_data = self.original_data
         # Display waveform
         self.plot.display_waveform(self)
 
-    def toggle_play_pause(self, event):
+    def toggle_play_pause(self, _):
         print("Toggling play/pause")
         if self.playing:
             self.stop_mp3()  # Pause the music
         else:
-            self.play_mp3() # Unpause the music
+            self.play_mp3()  # Unpause the music
 
     def play_mp3(self):
         pygame.mixer.quit()
@@ -89,7 +155,8 @@ class Core:
 
         if self.playing_data.any():
             if self.num_channels == 1:
-                audio = np.stack((self.playing_data, self.playing_data), axis=-1)
+                audio = np.stack(
+                    (self.playing_data, self.playing_data), axis=-1)
                 sound = pygame.mixer.Sound(buffer=audio)
 
                 # Play the sound in a loop
@@ -100,10 +167,9 @@ class Core:
                 print(f"Start time: {self.start_play_time}")
 
                 self.playing = True
-                self.plot.update_plot()  # Start the plot update loop   
+                self.plot.update_plot()  # Start the plot update loop
 
                 print("Playing")
-
 
     def get_current_time(self):
         return pygame.time.get_ticks() - self.start_play_time + self.loop_start
@@ -134,7 +200,7 @@ class Core:
     #             print(f"Start time: {self.start_play_time}")
 
     #             self.playing = True
-    #             self.plot.update_plot()  # Start the plot update loop   
+    #             self.plot.update_plot()  # Start the plot update loop
 
     #             print("Playing")
 
@@ -143,17 +209,18 @@ class Core:
 
     #             # Start a timer to call play_mp3 again after the sound finishes playing
     #             if not self.stopped:
-    #                 threading.Timer(duration, self.play_mp3).start()    
+    #                 threading.Timer(duration, self.play_mp3).start()
 
     def on_select(self, xmin, xmax):
         # Convert sample indices to time in milliseconds
-        self.loop_start = int((xmin  / self.sample_rate) * 1000)
-        self.loop_end = int((xmax  / self.sample_rate) * 1000)
-        print(f"Selected region from {self.loop_start} ms to {self.loop_end} ms")
+        self.loop_start = int((xmin / self.sample_rate) * 1000)
+        self.loop_end = int((xmax / self.sample_rate) * 1000)
+        print(f"Selected region from {
+              self.loop_start} ms to {self.loop_end} ms")
 
-        self.playing_data = get_audio_array(self.original_data, self.loop_start/1000, 
-                                       self.loop_end/1000 - self.loop_start/1000,
-                                       self.sample_rate)
+        self.playing_data = get_audio_array(self.original_data, self.loop_start/1000,
+                                            self.loop_end/1000 - self.loop_start/1000,
+                                            self.sample_rate)
         # if self.playing:
         #     self.play_mp3()
 
@@ -167,7 +234,8 @@ class Core:
 
     def on_plot_click(self, start_at):
         self.playing_data = get_audio_array(self.original_data, start_at/1000,
-                                            self.original_data.shape[0]/self.sample_rate - start_at/1000,
+                                            self.original_data.shape[0] /
+                                            self.sample_rate - start_at/1000,
                                             self.sample_rate)
         self.loop_start = start_at
         self.loop_end = None
@@ -187,7 +255,7 @@ class Core:
                 self.playing = False
                 self.paused_at = pygame.time.get_ticks()
 
-                self.plot.update_plot() 
+                self.plot.update_plot()
                 print(f"Paused at {self.paused_at}")
                 print("Paused")
             else:
@@ -201,7 +269,7 @@ class Core:
                     print(f"Start time: {self.start_play_time}")
 
                     self.playing = True
-                    self.plot.update_plot()  
+                    self.plot.update_plot()
                     print("Unpaused")
 
     def reset_loop(self):
@@ -209,26 +277,36 @@ class Core:
         self.loop_start = 0
         self.loop_end = self.playing_data.shape[0]
         if self.playing:
-            self.play_mp3()        
+            self.play_mp3()
         print("Loop reset")
 
     def slow_down(self):
+        """
+        Slows down the audio by a factor of 0.8.
+        """
         slow_down_rate = 0.8
-        stretched_audio_int16 = stretch_audio(self.original_data, slow_down_rate)  
+        stretched_audio_int16 = stretch_audio(
+            self.original_data, slow_down_rate)
 
-        self.original_data = stretched_audio_int16 
+        self.original_data = stretched_audio_int16
         self.plot.display_waveform(self)
-        
+
         self.playing_data = stretched_audio_int16
-     
+
         print(f"Slow down rate: {slow_down_rate}")
 
     def mark_beat(self, event):
+        """
+        Marks the current time as a beat.
+        """
         current_time = self.get_current_time()
         self.beats.append(current_time)
         # self.plot.update_beat_axis()
         # print(self.beats)
 
     def mark_measure(self, event):
+        """
+        Marks the current time as a measure.
+        """
         self.measures.append(self.get_current_time())
         print(self.measures)
--- plot.py
+++ plot.py
@@ -1,13 +1,12 @@
 import tkinter as tk
+import time
 import pygame
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.widgets import SpanSelector
 import matplotlib.ticker as ticker
-import time
 
-from core import get_audio_array
 
 class Plot:
     def __init__(self, root):
@@ -26,7 +25,8 @@ class Plot:
         # self.toolbar.update()
 
         # Span selector for interactive selection
-        self.span = SpanSelector(self.ax, self.on_select, 'horizontal', useblit=True)
+        self.span = SpanSelector(
+            self.ax, self.on_select, 'horizontal', useblit=True)
 
         # Reference to the core
         self.core = None
@@ -38,7 +38,7 @@ class Plot:
         self.playback_line = None
         self.loop_start_line = None
         self.loop_end_line = None
-        
+
         self.beat_axis = None
 
         self.plot_downsample = 20
@@ -48,7 +48,6 @@ class Plot:
         self.plot_line_index = 2
 
         self.current_plot_pos = 0
-
 
     def increase_loop_line(self):
         self.plot_line_index = self.plot_line_index + 1
@@ -62,7 +61,7 @@ class Plot:
         if self.plot_line_index < 1:
             self.plot_line_index = 1
         print(f"Plot line: {self.plot_line_index}")
-        self.draw_plot()    
+        self.draw_plot()
 
     def increase_loop_window(self):
         self.plot_window = 2*self.plot_window
@@ -73,7 +72,7 @@ class Plot:
 
     def decrease_loop_window(self):
         self.plot_window = int(self.plot_window/2)
-        print(f"Plot window: {self.plot_window}")      
+        print(f"Plot window: {self.plot_window}")
         self.draw_plot()
 
     def display_waveform(self, core):
@@ -82,10 +81,13 @@ class Plot:
         self.ax.clear()
         self.plot_line, = self.ax.plot(self.plot_data)
         # self.plot_window = len(self.plot_data)
-        self.playback_line = self.ax.axvline(x=0, color='r')  # Vertical line for playback position
+        # Vertical line for playback position
+        self.playback_line = self.ax.axvline(x=0, color='r')
 
-        self.loop_start_line = self.ax.axvline(x=0, color='r', linestyle='--', linewidth=0.5)  # Red dashed line for loop start
-        self.loop_end_line = self.ax.axvline(x=0, color='g', linestyle='--', linewidth=0.5)
+        self.loop_start_line = self.ax.axvline(
+            x=0, color='r', linestyle='--', linewidth=0.5)  # Red dashed line for loop start
+        self.loop_end_line = self.ax.axvline(
+            x=0, color='g', linestyle='--', linewidth=0.5)
 
         # Create a twin x-axis for the beats
         self.beat_axis = self.ax.twiny()
@@ -93,14 +95,17 @@ class Plot:
         self.beat_axis.xaxis.set_label_position('top')
 
         # Convert beats from milliseconds to samples
-        beat_samples = [(beat / 1000) * self.core.sample_rate for beat in self.core.beats]
+        beat_samples = [
+            (beat / 1000) * self.core.sample_rate for beat in self.core.beats]
 
         # Adjust for downsampling
-        beat_samples_downsampled = [beat / self.plot_downsample for beat in beat_samples]
+        beat_samples_downsampled = [
+            beat / self.plot_downsample for beat in beat_samples]
 
         # Set the initial tick locations to the beats
         self.beat_axis.set_xticks(beat_samples_downsampled)
-        self.beat_axis.set_xticklabels([''] * len(beat_samples_downsampled))  # No labels, just ticks
+        self.beat_axis.set_xticklabels(
+            [''] * len(beat_samples_downsampled))  # No labels, just ticks
 
         self.update_xaxis_labels()
         self.canvas.draw()
@@ -108,13 +113,14 @@ class Plot:
 
     def on_select(self, xmin, xmax):
         print(f"Selected region from {xmin} to {xmax}")
-        self.core.on_select(xmin * self.plot_downsample, xmax* self.plot_downsample)
+        self.core.on_select(xmin * self.plot_downsample,
+                            xmax * self.plot_downsample)
 
     def transform_to_ms(self, value, min_ms, max_ms):
         return value * (max_ms - min_ms) + min_ms
 
     def get_current_x_range(self, ax):
-        return ax.get_xlim()    
+        return ax.get_xlim()
 
     # Define a callback function to handle click events
     def on_click(self, event):
@@ -134,16 +140,17 @@ class Plot:
 
         # print(f"Clicked at x={xdata}")
         # self.core.on_click(xdata)
-        
-        x = event.xdata  * self.plot_downsample
+
+        x = event.xdata * self.plot_downsample
         start_at = int((x / self.core.sample_rate) * 1000)
 
         self.core.on_plot_click(start_at)
 
-        self.current_plot_pos = int(start_at / 1000 * self.core.sample_rate / self.plot_downsample)
+        self.current_plot_pos = int(
+            start_at / 1000 * self.core.sample_rate / self.plot_downsample)
         print(f"Clicked at x1={start_at}")
 
-        # self.plot.draw_plot()        
+        # self.plot.draw_plot()
 
     def update_plot(self):
         if self.plot_data is not None:
@@ -151,11 +158,13 @@ class Plot:
             current_time = self.core.get_current_time()
             # the current time should not exceed the loop end. It should loop back to the loop start
             if self.core.loop_end is not None and current_time >= self.core.loop_end:
-                current_time = self.core.loop_start + (current_time - self.core.loop_end)
+                current_time = self.core.loop_start + \
+                    (current_time - self.core.loop_end)
                 self.core.start_play_time = pygame.time.get_ticks()
 
             # convert the time to sample index
-            self.current_plot_pos = int(current_time / 1000 * self.core.sample_rate / self.plot_downsample)
+            self.current_plot_pos = int(
+                current_time / 1000 * self.core.sample_rate / self.plot_downsample)
 
             # self.current_plot_pos = self.current_plot_pos + self.loop_start
             self.draw_plot()
@@ -163,55 +172,61 @@ class Plot:
             if self.core.playing:
                 self.root.after(100, self.update_plot)
 
-
     def draw_plot(self):
-        start = max(0, self.current_plot_pos - int(self.plot_line_index * self.plot_window / self.plot_line_divisions))
-        end = min(len(self.plot_data), self.current_plot_pos + int((self.plot_line_divisions - self.plot_line_index)*self.plot_window/self.plot_line_divisions))
+        start = max(0, self.current_plot_pos - int(self.plot_line_index *
+                    self.plot_window / self.plot_line_divisions))
+        end = min(len(self.plot_data), self.current_plot_pos + int((self.plot_line_divisions -
+                  self.plot_line_index)*self.plot_window/self.plot_line_divisions))
 
         # start *= self.plot_downsample
         # end *= self.plot_downsample
 
-        self.plot_line.set_data(np.arange(start, end), self.plot_data[start:end])
+        self.plot_line.set_data(np.arange(start, end),
+                                self.plot_data[start:end])
         self.ax.set_xlim(start, end)
 
-        self.playback_line.set_xdata([self.current_plot_pos])  # Update playback position line
-
+        # Update playback position line
+        self.playback_line.set_xdata([self.current_plot_pos])
 
         if self.core.loop_start is not None:
-            loop_start_index = int(self.core.loop_start / 1000 * self.core.sample_rate / self.plot_downsample)
+            loop_start_index = int(
+                self.core.loop_start / 1000 * self.core.sample_rate / self.plot_downsample)
             self.loop_start_line.set_xdata([loop_start_index])
         else:
             self.loop_start_line.set_xdata([0])
         if self.core.loop_end is not None:
-            loop_end_index = int(self.core.loop_end / 1000 * self.core.sample_rate / self.plot_downsample)
+            loop_end_index = int(self.core.loop_end / 1000 *
+                                 self.core.sample_rate / self.plot_downsample)
             self.loop_end_line.set_xdata([loop_end_index])
         else:
             self.loop_end_line.set_xdata([0])
 
-        self.update_beat_axis()            
+        self.update_beat_axis()
 
         self.canvas.draw()
 
     def update_xaxis_labels(self):
         # Format the x-axis ticks to show minutes and seconds
-        formatter = ticker.FuncFormatter(lambda x, pos: time.strftime('%M:%S', time.gmtime(x / self.core.sample_rate * self.plot_downsample)))
+        formatter = ticker.FuncFormatter(lambda x, pos: time.strftime(
+            '%M:%S', time.gmtime(x / self.core.sample_rate * self.plot_downsample)))
         self.ax.xaxis.set_major_formatter(formatter)
 
         # Set the x-axis label to 'Time (min:sec)'
         self.ax.set_xlabel('Time (min:sec)')
 
         self.update_beat_axis()
-    
+
     def update_beat_axis(self):
         # print("Updating beat axis")
         # Update the beat axis
         if hasattr(self, 'beat_axis'):
             # print(self.beat_axis.get_xticks())
             # Convert beats from milliseconds to samples and adjust for downsampling
-            beat_samples_downsampled = [(beat / 1000) * self.core.sample_rate / self.plot_downsample for beat in self.core.beats]
+            beat_samples_downsampled = [
+                (beat / 1000) * self.core.sample_rate / self.plot_downsample for beat in self.core.beats]
             self.beat_axis.set_xticks(beat_samples_downsampled)
-            self.beat_axis.set_xticklabels([''] * len(beat_samples_downsampled))
-        
-            # Update the x-axis range of the beat axis to match the main x-axis
-            self.beat_axis.set_xlim(self.ax.get_xlim())            
+            self.beat_axis.set_xticklabels(
+                [''] * len(beat_samples_downsampled))
 
+            # Update the x-axis range of the beat axis to match the main x-axis
+            self.beat_axis.set_xlim(self.ax.get_xlim())
